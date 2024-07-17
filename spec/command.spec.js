957e77ad200611be33af28ddaf6beeb3d1517dfd
Alex <alex@Dominics-MacBook-Air.local>
2024-07-17 00:41:31 -0500

    Assignment 3 Task 3 Revision: I think my tests are working now

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -9,9 +9,11 @@ describe("Command class", function() {
     expect( function() { new Command();}).toThrow(new Error("Command type required."));
   });       
   it("constructor sets command type", function() {
-    expect(Command.commandType);
+    let modeCommand = new Command('MODE_CHANGE', 'LOW_POWER');
+    expect(modeCommand.commandType).toEqual('MODE_CHANGE');
   });
   it("constructor sets a value passed in as the 2nd argument", function() {
-    expect(Command.value);
+    let moveCommand = new Command('MOVE', 12000);
+    expect(moveCommand.value).toEqual(12000);
   });
   });
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,13 +5,16 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+    let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let message = new Message('Test message with two commands', commands);
+
     it("Throws error if a name is NOT passed into the constructor as the first parameter", function() {
         expect( function() { new Message();}).toThrow(new Error("name required."));
     });
     it("constructor sets name", function() {
-        expect(Message.name);
+        expect(message.name).toEqual('Test message with two commands');
     });
     it("contains a commands array passed into the constructor as the 2nd argument", function() {
-        expect(Message.commands);
+        expect(commands).toEqual([new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')]);
     });
 });
