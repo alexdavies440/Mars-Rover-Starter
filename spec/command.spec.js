a04490b546fe317acb820ae9fcf7c1fc60809956
Alex <alex@Dominics-MacBook-Air.local>
2024-07-16 20:45:40 -0500

    Assignment 3 Task 2: 2nd test passing?

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,7 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  it("constructor sets command type", function() {
+    Command.constructor.commandType})
 
 });
