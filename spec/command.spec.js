35f626582dbec5743b159d8140c6fb08e82b975e
Alex <alex@Dominics-MacBook-Air.local>
2024-07-16 21:18:17 -0500

    Assignment 3 Task 3 test 4

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -6,13 +6,13 @@ const Command = require('../command.js');
 describe("Command class", function() {
 
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
-    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
-  });
+    expect( function() { new Command();}).toThrow(new Error("Command type required."));
+  });       
   it("constructor sets command type", function() {
-    Command.constructor.commandType;
+    Command.constructor.commandType;    //I have no clue why this worked
   });
   it("constructor sets a value passed in as the 2nd argument", function() {
-    Command.constructor();
+    Command.constructor();            // I have no clue why this worked
   });
 
 });
